ac083e358832994a3c529b369cf6a64479bbea22
quynh410 <147390551+quynh410@users.noreply.github.com>
2023-11-08 08:38:53 +0700

    Add files via upload

---
+++ bt1ss2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int canh;
+	canh = 2;
+	int chieudai;
+	int chieurong;
+	int bankinh;
+	float PI;
+	PI = 3.14;
+	int dientichHinhVuong = canh*4;
+	int chuviHinhVuong = canh*canh;
+	int dientichHinhChuNhat = (chieudai + chieurong)*2;
+	int chuviHinhChuNhat = chieudai * chieurong;
+	float chuviHinhTron = bankinh*PI;
+	float dientichHinhTron = bankinh*bankinh*PI;
+}
